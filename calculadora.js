ef299ff87eb217ee4a6ff60c423520624914dea6
kaianepach <kaianepach1@gmail.com>
2022-04-01 09:15:02 -0300

    versão 1.1.1

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,12 @@
 var somar = require("./modulos/somar");
 
 console.log(somar(20,90));
+
+var subtrair = require("./modulos/subtrair");
+console.log(subtrair(20, 10));
+
+var multiplicar = require("./modulos/multiplicar");
+console.log(multiplicar(2, 4));
+
+ var dividir = require("./modulos/dividir");
+console.log(dividir(8, 2));
